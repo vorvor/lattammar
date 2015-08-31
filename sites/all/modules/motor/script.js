1f8047880f682d9fbfb9ca06d69b31868f4daf42
vorvor <vorosborisz@gmail.com>
2015-08-31 11:59:25 +0200

    geoloc

--- sites/all/modules/motor/script.js
+++ sites/all/modules/motor/script.js
@@ -1,7 +1,27 @@
 (function ($) {
+  
+  function GEOprocess(position) {
+      // update the page to show we have the lat and long and explain what we do next
+    $('body').prepend('Latitude: ' + position.coords.latitude + ' Longitude: ' + position.coords.longitude);
+      
+  }
+  
+  // this is used when the visitor bottles it and hits the "Don't Share" option
+  function GEOdeclined(error) {
+    document.getElementById('geo').innerHTML = 'Error: ' + error.message;
+  }
+
+
   $(document).ready(function($) {
-    
    
+
+   
+    
+   if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(GEOprocess, GEOdeclined);
+    }else{
+      document.getElementById('geo').innerHTML = 'Your browser sucks. Upgrade it.';
+    }
     
     
     
