3b4d9c2b2538ece6dce7fac81355af582da6cece
vorvor <vorosborisz@gmail.com>
2015-08-31 13:53:56 +0200

    geoMOB

--- sites/all/modules/motor/script.js
+++ sites/all/modules/motor/script.js
@@ -2,8 +2,10 @@
   
   function GEOprocess(position) {
       // update the page to show we have the lat and long and explain what we do next
-    //$('body').prepend('Latitude: ' + position.coords.latitude + ' Longitude: ' + position.coords.longitude);
-      
+    $('#geoMOB').prepend('Latitude: ' + position.coords.latitude + ' Longitude: ' + position.coords.longitude);
+    $('#edit-field-point-und-0-geom-lat').val(position.coords.latitude);
+    $('#edit-field-point-und-0-geom-lon').val(position.coords.longitude);
+    $('#tipp-node-form').submit();
   }
   
   // this is used when the visitor bottles it and hits the "Don't Share" option
@@ -11,19 +13,25 @@
     document.getElementById('geo').innerHTML = 'Error: ' + error.message;
   }
 
+  function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+  }
 
-  $(document).ready(function($) {
-   
-
-   
-   /* 
-   if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(GEOprocess, GEOdeclined);
-    }else{
+  function geoMOB() {
+    if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(GEOprocess, GEOdeclined);
+    } else {
       document.getElementById('geo').innerHTML = 'Your browser sucks. Upgrade it.';
     }
-    */
+  }
+  
+  $(document).ready(function($) {
+   
+    $('#geoMOB').click(function() {
+      geoMOB();
+    });
     
-    
-  });
+    });
 }) (jQuery);
